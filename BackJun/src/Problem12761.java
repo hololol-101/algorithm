411a778f7f2abfc0307763c871931890bd580a87
hololol-101 <shrkddms1010@naver.com>
2023-08-16 23:56:29 +0900

    [BOJ] 돌다리 / 실버 1 https://www.acmicpc.net/problem/12761 Map 대신 Array 사

--- BackJun/src/Problem12761.java
+++ BackJun/src/Problem12761.java
@@ -17,51 +17,54 @@ public class Problem12761 {
 		int b = Integer.parseInt(s[1]);
 		int dong = Integer.parseInt(s[2]);
 		int joo = Integer.parseInt(s[3]);
-		int answer = 0;
+		int []map = new int[100001];
+		
 		Queue<Integer> que = new LinkedList<>();
-		Map<Integer, Integer> map = new HashMap<>();
-		int[] dir = {a, b, 1, -1, a*-1, b*-1};
 		
 		que.offer(dong);
-		map.put(dong, 0);
-
+		map[dong]=0;
 		//solution
-		//bfs
-		while(!que.isEmpty()) {
+		//dfs
+		while(!que.isEmpty()&&map[joo]==0) {
 			int idx = que.poll();
-			int cnt = map.get(idx)+1;
 			int nx = 0;
-			for(int i=0; i<2; i++) {
-				nx = idx*dir[i];
-				if(nx>100000) {
-					continue;
-				}
+			for(int i=0; i<8; i++) {
+				switch(i) {
+				case 0:
+					nx = idx*a;
+					break;
+				case 1:
+					nx = idx*b;
+					break;
+				case 2:
+					nx = idx+a;
+					break;
+				case 3: 
+					nx = idx+b;
+					break;
+				case 4:
+					nx = idx+1;
+					break;
+				case 5:
+					nx = idx-1;
+					break;
+				case 6:
+					nx = idx-a;
+					break;
+				case 7:
+					nx = idx-b;
+					break;
 				
-				if(map.get(nx)==null) {
+				}
+				if(nx<100001&&nx>=0&&map[nx]==0) {
+					map[nx]= map[idx]+1;
 					que.offer(nx);
-					map.put(nx, cnt);					
 				}
-			}
-			
-			for(int i:dir) {
-				nx = idx+i;
-				if(nx>100000||nx<0) {
-					continue;
-				}
-				if(map.get(nx)==null) {
-					que.offer(nx);
-					map.put(nx, cnt);					
-				}
-			}
-			
-			if(map.get(joo)!=null) {
-				answer = map.get(joo);
-				break;
 			}
 		}
 		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.append(answer+"");
+		bw.append(map[joo]+"");
 		bw.flush();
 		bw.close();
 		
