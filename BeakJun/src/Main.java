ddd90b1997f83c821baf06cad5e277c664257b51
김첨지 <gangeunno@gmail.com>
2024-12-29 04:12:34 +0900

    Input 방식 추가

--- BeakJun/src/Main.java
+++ BeakJun/src/Main.java
@@ -5,15 +5,18 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class Main {
-	private static final int CASE = 4;
-	
+
 	public static void main(String[] args) throws IOException {
 		
 		// input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		br.readLine();
 		br.close();
-				
+
+		//input 2 - 숫자와 스페이스, 엔터로만 이루어져 있을때
+		readInt();
+
+
 		//test	
 				
 		// output 
@@ -21,6 +24,19 @@ public class Main {
 		bw.append("");
 		bw.flush();
 		bw.close();
+
 	}
 
+	static int readInt() throws IOException {
+
+		int c, n = System.in.read() & 15;
+
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+		return isNegative ? ~n + 1 : n;
+
+	}
 }
